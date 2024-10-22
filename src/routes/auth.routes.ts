71d02f8a433d852411d4c7e478028263e2480570
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-10-22 22:06:49 +0700

    refactor: Add authentication bypass for registration and login routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,16 +2,22 @@ import { ServerRoute } from "@hapi/hapi";
 import AuthController from "../controllers/auth.controller";
 
 const authRoutes: ServerRoute[] = [
-  {
-    method: "POST",
-    path: "/api/auth/register",
-    handler: AuthController.register,
-  },
-  {
-    method: "POST",
-    path: "/api/auth/login",
-    handler: AuthController.login,
-  },
+    {
+        method: "POST",
+        path: "/api/auth/register",
+        handler: AuthController.register,
+        options: {
+            auth: false,
+        },
+    },
+    {
+        method: "POST",
+        path: "/api/auth/login",
+        handler: AuthController.login,
+        options: {
+            auth: false,
+        },
+    },
 ];
 
 export default authRoutes;
