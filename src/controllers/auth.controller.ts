11ba498ec7bc0c6298ec63a3670c985c4a9b9547
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-10-22 21:06:39 +0700

    refactor: add response and validation, refactor code

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,19 +1,48 @@
 import { Request, ResponseToolkit } from "@hapi/hapi";
 import AuthService from "../services/auth.service";
 import logger from "../utils/logger";
+import * as responseHelper from "../helpers/response.helper";
+import { userValidation, loginValidation } from "../helpers/validation.helper";
+
 
 class AuthController {
-  static async register(request: Request, h: ResponseToolkit) {
-    const { username, password, name } = request.payload as any;
-    const result = await AuthService.register(username, password, name);
-    return h.response(result).code(201);
-  }
+    static async register(request: Request, h: ResponseToolkit) {
+        try {
+            const isValid = userValidation(request.payload);
+            if (isValid) {
+                return h.response(responseHelper.errorMessage(400, isValid)).code(400);
+            }
+            const { username, password, name } = request.payload as any;
+            const result = await AuthService.register(username, password, name);
+            if (result) {
+                return h.response(responseHelper.successMessage(201, "Successfully to register user")).code(201);
+            } else {
+                return h.response(responseHelper.errorMessage(400, "Failed to register user")).code(400);
+            }            
+        } catch (error) {
+            logger.error(`${error}`)
+            return h.response(responseHelper.errorMessage(400, `${error}`)).code(400);
+        }
+    }
 
-  static async login(request: Request, h: ResponseToolkit) {
-    const { username, password } = request.payload as any;
-    const result = await AuthService.login(username, password);
-    return h.response(result).code(200);
-  }
+    static async login(request: Request, h: ResponseToolkit) {
+        try {
+            const isValid = loginValidation(request.payload)
+            if (isValid) {
+                return h.response(responseHelper.errorMessage(400, isValid)).code(400);
+            }
+            const { username, password } = request.payload as any;
+            const result = await AuthService.login(username, password);
+            if (result) {
+                return h.response(responseHelper.successData(200, result)).code(200);
+            } else {
+                return h.response(responseHelper.errorMessage(400, "Invalid credentials")).code(400);
+            }
+        } catch (error) {
+            logger.error(`${error}`)
+            return h.response(responseHelper.errorMessage(400, `${error}`)).code(400);
+        }
+    }
 }
 
 export default AuthController;
