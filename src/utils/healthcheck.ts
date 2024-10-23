2751ddbc24bbe923dc978bc83da2d38b4785ee09
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-10-23 21:43:39 +0700

    refactor: Update health check and readiness check routes

--- src/utils/healthcheck.ts
+++ src/utils/healthcheck.ts
@@ -1,11 +1,31 @@
-import { ServerRoute } from "@hapi/hapi";
+import { ServerRoute, Request, ResponseToolkit } from "@hapi/hapi";
+import pool from "./database";
+import * as responseHelper from "../helpers/response.helper"
 
+// Health Check route
 const healthCheckRoute: ServerRoute = {
-  method: "GET",
-  path: "/health",
-  handler: (request, h) => {
-    return h.response({ status: "ok" }).code(200);
-  },
+    method: "GET",
+    path: "/health",
+    handler: (request, h) => {
+        // return h.response({ status: "ok" }).code(200);
+        return h.response(responseHelper.successMessage("Health Check OK", 200)).code(200);
+    },
 };
 
-export default healthCheckRoute;
+// Readiness Check route
+const readinessCheckRoute: ServerRoute = {
+    method: "GET",
+    path: "/readiness",
+    handler: async (request: Request, h: ResponseToolkit) => {
+        try {
+            // Check the database connection
+            await pool.query("SELECT 1");
+            return h.response(responseHelper.successMessage("Readiness Check OK", 200)).code(200);
+        } catch (error) {
+            console.error("Readiness check failed:", error);
+            return h.response(responseHelper.errorMessage("Readiness Check Failed", `${error}`, 503)).code(503);
+        }
+    },
+};
+
+export { healthCheckRoute, readinessCheckRoute };
